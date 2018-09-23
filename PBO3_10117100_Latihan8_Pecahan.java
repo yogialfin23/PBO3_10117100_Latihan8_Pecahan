55f0adf17dfdf638994084a5d9791d0791e0c247
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:44:14 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan8_Pecahan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan8_pecahan;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float : " +f);
+        System.out.println("double : " +d);
+    }
+    
+}
